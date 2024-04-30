51d23491906c9a093c0d1a93f155a7eeb05842d8
DevCache1 <martynasbaranskas@gmail.com>
2024-04-30 12:42:51 +0300

    Initial Commit

---
+++ FrenchCards/FrenchCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchCardsApp.swift
+//  FrenchCards
+//
+//  Created by Martynas Baranskas on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FrenchCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
